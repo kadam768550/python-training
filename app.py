fb88aee43380a208b4b3ef161307741f06c54a23
kadam768550 <gangaprasadkadam@gmail.com>
2026-06-08 00:17:07 +0530

    Day 5:single page flask

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Welcome to My project</h1>'
+
+if __name__ == '__main__':
+    app.run(debuge=True)
+    
