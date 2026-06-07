fd05d5c6e1107d829a2978f02ce27fdb189f6758
kadam768550 <gangaprasadkadam10@gmail.com>
2026-06-07 18:29:53 +0530

    Day 4 program

---
+++ test.py
@@ -0,0 +1,8 @@
+name ="Rahul"
+marks = 85
+city = "Hingoli"
+
+#Method 1
+print ("hello " + name + "!" + " Your marks are " + str(marks) + " and you are from " + city + ".")
+#Method 2 
+print (f"Hello {name}! Your marks are {marks} and you are from {city}.")
