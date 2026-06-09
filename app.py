fba7a65dd187eced88e6241ce8a7455c53039582
kadam768550 <gangaprasadkadam@gmail.com>
2026-06-09 15:17:20 +0530

    Day 6: flask render template

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+prod = [
+    {"name": "Laptop", "price": 50000, "quality": "Good"},
+    {"name": "Mouse", "price": 1000, "quality": "Excellent"},
+    {"name": "Keyboard", "price": 2000, "quality": "Average"},
+    {"name": "Smartphone", "price": 30000, "quality": "Good"}
+]
+
 @app.route('/')
 def home():
-    return '<h1>Welcome to My project</h1>'
+    return render_template('home.html',products=prod,latest_products=prod[:3])
+@app.route('/sales')
+def sales():
+    return render_template('Sales.html')
+@app.route('/suppliers')
+def suppliers():
+    return render_template('Suppliers.html')
+@app.route('/products')
+def product_list():
+    return render_template('products.html',products=prod)
 
 if __name__ == '__main__':
-    app.run(debuge=True)
-    
+    app.run(debug=True)
+
