1a22670eca40c973bf84eef64a4c31f269497e4d
kadam768550 <gangaprasadkadam@gmail.com>
2026-06-18 12:52:31 +0530

    Day 7: Bootstrap navbar cards table badges

--- marks.py
+++ marks.py
@@ -9,23 +9,13 @@ s4 = float(input("Enter marks for Subject 4: "))
 s5 = float(input("Enter marks for Subject 5: "))
 
 
-
-
-
 total = s1 + s2 + s3 + s4 + s5
 
 percentage = total / 5
 
-
-
-
-
 print("Total Marks:", total)
 
 print("Percentage:", percentage)
-
-
-
 
 
 if percentage >= 75:
