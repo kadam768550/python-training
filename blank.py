4ff246ce6331c1c15987a9c5fbc9a0530462f2c7
kadam768550 <gangaprasadkadam@gmail.com>
2026-07-20 16:56:24 +0530

    Day 9:  SQLITE database for project

--- blank.py
+++ blank.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, flash
+
 app = Flask(__name__)
+
 app.secret_key = 'my secret key' # Needed for flashing messages
+
 @app.route("/")
 def home():
     return '<h1>My Project</h1>'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
