7f82a12573cbd2401f4fd6907f4d279863683c2b
kadam768550 <gangaprasadkadam@gmail.com>
2026-07-26 20:58:55 +0530

    fix: use absolute path for database

--- database.py
+++ database.py
@@ -1,16 +1,19 @@
+import os
 import sqlite3
 from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 app.secret_key = "my secret key"
 
-
+#Absoulute path - Always with app.py folder
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, 'myproject.db')
+# 2 functions
 def get_db():
     """Database connection"""
-    conn = sqlite3.connect("myproject.db")
-    conn.row_factory = sqlite3.Row
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row # To access columns by name
     return conn
-
 
 def init_db():
     """Create tables"""
